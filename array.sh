bad9e244f6ed0c81fe2d34f68e02b1f5b0ffaaa6
Evans Kiprotich Kiplagat <35095785+evansMeja@users.noreply.github.com>
2018-11-25 02:22:22 +0300

    Create array.sh

---
+++ array.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+james[0]=5
+james[1]=89
+
+juma[0]=74
+juma[1]=90
+
+arr[0]={$james,$juma}
+arr[1]=evans
+arr[2]=89
+
+#for i in {0..2}
+#do
+#echo $(arr[$i])
+#done
+
+echo "${arr[0] }"
